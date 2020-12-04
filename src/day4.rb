2eefb8a9d0c1c4ae67bf01c40bd660b83af2123f
Ushma Kapure <ukapure@sfu.ca>
2020-12-04 15:27:22 -0800

    refactor day 4 part 2

--- src/day4.rb
+++ src/day4.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'pry'
-
 # Puzzle: https://adventofcode.com/2020/day/3
 
 def parse(source_file)
@@ -10,74 +8,22 @@ end
 
 def conditions
   {
-    byr: {
-      lower: 1920,
-      upper: 2002,
-      regex: /^[[:digit:]]{4}$/
-    },
-    iyr: {
-      regex: /^[[:digit:]]{4}$/,
-      lower: 2010,
-      upper: 2020
-    },
-    eyr: {
-      regex: /^[[:digit:]]{4}$/,
-      lower: 2020,
-      upper: 2030
-    },
-    hgt: {
-      regex: /^[[:digit:]]{2,3}(cm|in)$/,
-      cm: {
-        lower: 150,
-        upper: 193
-      },
-      in: {
-        lower: 59,
-        upper: 76
-      }
-    },
-    hcl: {
-      regex: /^#([[:digit:]]|[a-f]){6}$/
-    },
-    ecl: {
-      regex: /^(amb|blu|brn|gry|grn|hzl|oth)$/
-    },
-    pid: {
-      regex: /^[[:digit:]]{9}$/
-    }
+    byr: /^(19[2-9][0-9]|200[0-2])$/,
+    iyr: /^(201[0-9]|2020)$/,
+    eyr: /^(202[0-9]|2030)$/,
+    hgt: /^(59in|6[0-9]in|7[0-6]in|1[5-8][0-9]cm|19[0-3]cm)$/,
+    hcl: /^#([[:digit:]]|[a-f]){6}$/,
+    ecl: /^(amb|blu|brn|gry|grn|hzl|oth)$/,
+    pid: /^[[:digit:]]{9}$/
   }
-end
-
-def validate_key(key, str)
-  key = key.to_sym
-  regex = conditions[key][:regex]
-  return false unless str.match?(regex)
-
-  return true if [:hcl, :ecl, :pid].include?(key)
-
-  if key == :hgt
-    if str.include?('cm')
-      num = str.gsub('cm', '').to_i
-      return conditions[key][:cm][:lower] <= num && num <= conditions[key][:cm][:upper]
-    else
-      num = str.gsub('in', '').to_i
-      return conditions[key][:in][:lower] <= num && num <= conditions[key][:in][:upper]
-    end
-  else
-    num = str.to_i
-    return conditions[key][:lower] <= num && num <= conditions[key][:upper]
-  end
 end
 
 def validate_passport(entry)
   hash = entry.to_h { |i| i.split(':') }
-  conditions.each do |condition, _| 
-    return false unless hash.key?(condition.to_s) 
-    x = validate_key(condition, hash[condition.to_s])
-
-    # puts "Condition: #{condition}, value: #{hash[condition.to_s]}, valid: #{x}"
-    return false unless x
-  end 
+  conditions.each do |condition, regex|
+    return false unless hash.key?(condition.to_s)
+    return false unless hash[condition.to_s].match?(regex)
+  end
 
   true
 end
@@ -89,9 +35,3 @@ end
 
 # puts run('../input/day4-example.txt')
 puts run('../input/day4.txt')
-
-# puts validate_key(:pid, "000000001")
-# puts validate_key(:pid, "0123456789")
-
-
-
