28b2ee8ec34ff09c5a1328187e1d87dcf6360b9d
Ushma Kapure <ushma.kapure@gmail.com>
2022-12-02 15:15:53 -0800

    day 2 2022 - % math sol

--- 2022/day2/solution1.rb
+++ 2022/day2/solution1.rb
@@ -1,50 +1,27 @@
 input = File.readlines('./sample.txt')
 
 def outcome(opp, me)
-  return 'draw' if opp == me
-
-  case opp
-  when 'rock'
-    me == 'paper' ? 'win' : 'lose'
-  when 'paper'
-    me == 'scissors' ? 'win' : 'lose'
-  when 'scissors'
-    me == 'rock' ? 'win' : 'lose'
+  case ((me.ord - opp.ord) % 3).abs
+  when 0
+    'win'
+  when 1
+    'lose'
+  when 2
+    'draw'
   end
 end
 
 def score(outcome, item)
-  outcome_scores = {
-    'win' => 6,
-    'draw' => 3,
-    'lose' => 0
-  }
-  item_played = {
-    'X' => 1,
-    'Y' => 2,
-    'Z' => 3
-  }
+  outcome_scores = { 'win' => 6, 'draw' => 3, 'lose' => 0 }
+  item_played = { 'X' => 1, 'Y' => 2, 'Z' => 3 }
 
   outcome_scores[outcome] + item_played[item]
 end
 
 score = 0
-
-opp_key_lookup = {
-  'A' => 'rock',
-  'B' => 'paper',
-  'C' => 'scissors'
-}
-
-me_key_lookup = {
-  'X' => 'rock',
-  'Y' => 'paper',
-  'Z' => 'scissors'
-}
-
 input.each do |game|
   opp, me = game.split(' ')
-  result = outcome(opp_key_lookup[opp], me_key_lookup[me])
+  result = outcome(opp, me)
 
   score += score(result, me)
 end
--- 2022/day2/solution2.rb
+++ 2022/day2/solution2.rb
@@ -1,33 +1,25 @@
-input = File.readlines('./sample.txt')
+input = File.readlines('./input.txt')
+
+## Here we make A = rock, B = paper and C = scissors for everyone
+# X, Y, Z represent game results exclusively
 
 def score(outcome, item)
   outcome_scores = {
-    'win' => 6,
-    'draw' => 3,
-    'lose' => 0
+    2 => 6, # win
+    0 => 3, # draw
+    1 => 0  # lose
   }
-  item_played = {
-    'X' => 1,
-    'Y' => 2,
-    'Z' => 3
-  }
+  item_played = { 'A' => 1, 'B' => 2, 'C' => 3 }
 
   outcome_scores[outcome] + item_played[item]
 end
 
 def play(opp, outcome)
-  draw = {   'A' => 'X',    'B' => 'Y',    'C' => 'Z'  }
-  win = {    'A' => 'Y',    'B' => 'Z',    'C' => 'X'  }
-  lose = {   'A' => 'Z',    'B' => 'X',    'C' => 'Y'  }
+  options = %w[A B C]
+  raw_index = opp.ord - 'A'.ord - outcome # doing - because + didn't work as expected lol
+  index = (raw_index + 3) % 3 # +3 to ensure indices are positive, %3 to circularize
 
-  case outcome
-  when 'draw'
-    draw[opp]
-  when 'win'
-    win[opp]
-  when 'lose'
-    lose[opp]
-  end
+  options[index]
 end
 
 score = 0
@@ -36,9 +28,9 @@ input.each do |game|
   opp, letter = game.split(' ')
 
   item_played = {
-    'X' => 'lose',
-    'Y' => 'draw',
-    'Z' => 'win'
+    'X' => 1, # lose
+    'Y' => 0, # draw
+    'Z' => 2 # win
   }
   outcome = item_played[letter]
   me = play(opp, outcome)
